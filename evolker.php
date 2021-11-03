8469327f2e73fd77ab259d507efc4365825934bb
AntonyOnScript <santhiagoafk@gmail.com>
2021-11-03 09:17:59 -0300

    Prototipou funcionamento geral

--- evolker.php
+++ evolker.php
@@ -3,4 +3,39 @@
  * Plugin Name: Evolker
  * Version: 1.0beta
  */
+
+function adicionarVersaoBetaNoTopo()
+{
+    ?>
+
+    <style>
+        .aviso-dev {
+            display: flex;
+            max-width: 100%;
+            width: 100%;
+            background-color: rgba(0, 128, 0, 0.28);
+            justify-content: center;
+        }
+
+        .aviso-dev p a {
+            color: #024902;
+        }
+
+        .aviso-dev p a:hover {
+            color: #025d02;
+        }
+
+    </style>
+
+    <div class="aviso-dev">
+        <p class="color:red">
+            <a href="https://canaltech.com.br/produtos/O-que-significa-dizer-que-um-software-ou-produto-esta-em-versao-beta/" target="_blank">Beta v0.6 (24/10/2021 - 15h20)</a>
+        </p>
+    </div>
+
+    <?php
+}
+
+add_action('wp_head', 'adicionarVersaoBetaNoTopo');
+
 ?>
