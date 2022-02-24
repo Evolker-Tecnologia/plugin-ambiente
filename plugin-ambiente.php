fc1588d8c94959360ca0856b5f94460085e7d71c
Pedro Henrique <ph.camargo23@gmail.com>
2022-02-24 08:27:43 -0300

    Refatorou código.

--- plugin-ambiente.php
+++ plugin-ambiente.php
@@ -7,9 +7,8 @@
  * Author URI:  https://evolker.com.br
  */
 
-session_start();
-require_once "inc/conexao.pdo.php";
 require_once "inc/utils.php";
+require_once "inc/conexao.pdo.php";
 require_once "hooks/plugin-page.admin.php";
 
 function aoAtivarPlugin() { // ANTONY: por algum motivo a variavel $con aparece como NULL e por isso dá erro na hora de ativar o plugin
@@ -18,7 +17,46 @@ function aoAtivarPlugin() { // ANTONY: por algum motivo a variavel $con aparece 
     dbDelta($sql);
 }
 
-function adicionarBarraSuperior() {
+function pegarCorParaLocalAtual() {
+    $localAtual = verificarUrl();
+
+    if ($localAtual === "localhost") {
+        return ["corDeFundo" => "rgba(241, 196, 15, .6)", "corDoTexto" => "#494802", "corDoTextoHover" => "#5d4b02"];
+    } else if ($localAtual === "dev.evolker") {
+        return ["corDeFundo" => "rgba(46, 204, 113, .6)", "corDoTexto" => "rgb(0 42 10)", "corDoTextoHover" => "rgb(0 42 10)"];
+    } else if ($localAtual === "hom.evolker") {
+        return ["corDeFundo" => "rgba(52, 152, 219, .6)", "corDoTexto" => "#020849", "corDoTextoHover" => "#05025d"];
+    }
+}
+
+function pegarLocalAtual() {
+    $urlAtual = verificarUrl();
+
+    if ($urlAtual === "dev.evolker") {
+        return "Dev";
+    } else if ($urlAtual === "hom.evolker") {
+        return "Homologação";
+    } else if ($urlAtual === "localhost") {
+        return "Local";
+    }
+}
+
+function verificarUrl() {
+    $url = retornarUrl();
+    $locais = ["localhost", "dev.evolker", "hom.evolker"];
+    global $localAtual;
+
+    foreach ($locais as $local) {
+        if (strpos($url, $local)) {
+            $localAtual = $local;
+            break;
+        }
+    }
+
+    return $localAtual;
+}
+
+function rendezarVisualizacao() {
     $localAtual = pegarLocalAtual();
     $corLocalAtual = pegarCorParaLocalAtual();
     $temaAtualPasta = wp_get_theme()->get_stylesheet();
@@ -198,12 +236,8 @@ function adicionarBarraSuperior() {
 <?php
 }
 
-if (!is_admin()) {
-    add_action('wp_head', 'adicionarBarraSuperior');
-} else {
-    add_action('wp_before_admin_bar_render', 'adicionarBarraSuperior');
-}
-
 register_activation_hook(__FILE__, "aoAtivarPlugin");
+session_start();
+rendezarVisualizacao();
 
 ?>
